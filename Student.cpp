bd6b751c580b2ff007b729440648b16edcdf6c44
CJBake0430 <36430685+CJBake0430@users.noreply.github.com>
2018-02-26 21:07:03 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -21,26 +21,28 @@ string Student::getCWID() {
 void Student::addCourseGrade (const string &courseName, char grade) {
 	this -> courseName[courseNum] = courseName;
 	this -> grade[courseNum] = grade;
-	//courseNum++ ??
+	//add courseNum to advance to next grade
+	courseNum++;
 }
 
 double Student::getGPA() {
 	double value = 0.0;
 	for(int i = 0; i<courseNum; i++){
-	if(grade[i] = 'A'){
-		value = 4;
-	}
-	if(grade[i] = 'B'){
-		value = 3;
-	}
-	if(grade[i] = 'C'){
-		value = 2;
-	}
-	if(grade[i] = 'D'){
-		value = 1;
-	}
-	if(grade[i] = 'F'){
-		value = 0;
+		if(grade[i] = 'A'){
+			value += 4;
+		}
+		if(grade[i] = 'B'){
+			value += 3;
+		}
+		if(grade[i] = 'C'){
+			value += 2;
+		}
+		if(grade[i] = 'D'){
+			value += 1;
+		}
+		if(grade[i] = 'F'){
+			value += 0;
+		}
 	}
 	//FINISH THIS
 	return value/courseNum;
